4e907fe1f9972e6aecaf3e7fb0e081729c45a41d
Mitermayer18 <jardelska@gmail.com>
2019-10-08 02:39:28 -0300

    back pronto

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://omni:omni@aircnc-shard-00-00-9kjqs.mongodb.net:27017,aircnc-shard-00-01-9kjqs.mongodb.net:27017,aircnc-shard-00-02-9kjqs.mongodb.net:27017/aircnc?ssl=true&replicaSet=aircnc-shard-0&authSource=admin&retryWrites=true&w=majority' ,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//GET,POST,PUT,DELETE
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição,delete )
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
